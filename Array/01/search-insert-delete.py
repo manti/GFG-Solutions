9c71f385bc44494535305c40486a73291636cfc6
manti <manti@practo.com>
2017-07-03 21:24:38 +0530

    changed indentation type to space

--- Array/01/search-insert-delete.py
+++ Array/01/search-insert-delete.py
@@ -1,19 +1,19 @@
 #search Element function
 def findElement(array, element):
-	array_length = len(array)
-	for i in range(array_length):
-		if (array[i] == element):
-			return i
-	return -1
+    array_length = len(array)
+    for i in range(array_length):
+        if (array[i] == element):
+            return i
+    return -1
 
 
 # insert element function
 def insertElement(array, element):
-	array.append(element)
+    array.append(element)
 
 # remove element function
 def removeElement(array, element):
-	array.remove(element)
+    array.remove(element)
 
 
 array = [12, 34, 10, 6, 40, 30 , 13, 45]
@@ -22,9 +22,9 @@ element = 30
 #search operation
 index = findElement(array, element)
 if index != -1:
-	print "element found at position: " + str(index + 1 )
+    print "element found at position: " + str(index + 1 )
 else:
-	print "element not found"
+    print "element not found"
 
 
 #deletion operation
